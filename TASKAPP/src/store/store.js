a88659b4d93e8267d66b32eb6a5ca34bf1d411d7
divyansh-bhatnagar <97219781+divyansh-bhatnagar@users.noreply.github.com>
2022-08-30 15:24:56 +0530

    Delete TASKAPP directory

--- TASKAPP/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import reducer from "./reducer";
-
-const rootReducer = combineReducers({reducer: reducer});
-const Store = createStore(rootReducer);
-
-export default Store;
