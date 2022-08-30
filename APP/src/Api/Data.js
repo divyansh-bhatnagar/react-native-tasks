b6eca42b88151164af727220b7739707633ee4e8
divyansh-bhatnagar <97219781+divyansh-bhatnagar@users.noreply.github.com>
2022-08-30 15:23:39 +0530

    Delete APP directory

--- APP/src/Api/Data.js
+++
@@ -1,1 +0,0 @@
-export default baseUrl = 'https://jsonplaceholder.typicode.com';
--- APP/src/Store/store.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import reducer from "./reducer";
-
-const rootReducer = combineReducers({reducer: reducer});
-const store = createStore(rootReducer);
-
-export default store;
